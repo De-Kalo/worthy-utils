bac19ae71c65a97ff330d7c772036eb8c2ce643d
Noam Shabat <noam.shabat@worthy.com>
2019-12-29 05:28:06 -0800

    Add tagger

--- tagger.sh
+++ tagger.sh
@@ -1,23 +1,30 @@
 #!/bin/bash -x
 
+# first fetch so that tags will be updated.
 git fetch
+# get latest tag 
 tag=$(git log --decorate --oneline | grep "tag:" | head -n 1 | sed -E 's/[a-f0-9]{7,8} \(.*tag: ([0-9]{1,3}\.[0-9]{1,3}(\.[0-9]{1,4}){0,1}).*\).*/\1/')
+# get latest commit message
 message=$(git log  --pretty=format:'%s')
 
+# split tag tp array using separator '.'
 IFS='.' # space is set as delimiter
 read -ra verpart <<< "$tag"
 IFS=''
 
+# get major and minor
 MAJOR=${verpart[0]}
 MINOR=${verpart[1]}
 
 arraylen=${#verpart[@]}
 
+# get patch
 PATCH=0
 if (( arraylen > 2 )); then
   PATCH=${verpart[2]}
 fi
 
+# increment correct part of the version according to user request.
 if [ "$1" == "--major" ] ; then
    MAJOR=$((MAJOR+1))
    MINOR=0
