49440009eb69229cb66e0fcb8549ff587d423bb7
Noam Shabat <noam.shabat@worthy.com>
2021-07-21 14:10:25 +0300

    Add comment to tagger.sh

--- tagger.sh
+++ tagger.sh
@@ -1,8 +1,21 @@
-#!/bin/bash 
+#!/bin/bash
+
+# This file does the following:
+#  1. git fetch
+#  2. get's the last tag in the repository - assuming the tag is called x.y.z (major.minor.patch)
+#  3. creates a new tag based on the input
+#  3.1 by default creates a new minor version
+#  3.2 if the file is run with the '--patch' argument - creates a new patch version.
+#  3.3 if the file is run with the '--major' argument - creates a new major version.
+#
+# This file can be run 'directly' from the web by using the following command:
+# curl -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/De-Kalo/worthy-utils/master/tagger.sh | bash -s
+# or with arguments:
+# curl -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/De-Kalo/worthy-utils/master/tagger.sh | bash -s -- --patch
 
 # first fetch so that tags will be updated.
 git fetch
-# get latest tag 
+# get latest tag
 tag=$(git log --decorate --oneline -n1000| grep "tag:" | head -n 1 | sed -E 's/[a-f0-9]{7,12} \(.*tag: ([0-9]{1,3}\.[0-9]{1,3}(\.[0-9]{1,4}){0,1}).*\).*/\1/')
 # get latest commit message
 message=$(git log -n1 --pretty=format:'%s')
@@ -28,7 +41,7 @@ fi
 if [ "$1" == "--major" ] ; then
    MAJOR=$((MAJOR+1))
    MINOR=0
-   PATCH=0 
+   PATCH=0
 elif [ "$1" == "--patch" ] ; then
    PATCH=$((PATCH+1))
 else
